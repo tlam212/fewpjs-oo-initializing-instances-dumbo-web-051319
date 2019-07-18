7f04d13ab3c678556965b979b65e64088e1934b8
Toni Lamont <tlam212@gmail.com>
2019-07-18 03:20:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,20 @@ class Breakfast {
   }
   
 }
+
+class Lunch{
+  constructor(salad, soup, drink){
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner{
+  constructor(salad, soup, entree, dessert){
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
+}
